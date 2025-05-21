b47adac0cba45ee7bae5896f9d1cf5dd804258b2
463465810cz <48750880+zhengchen1999@users.noreply.github.com>
2025-05-21 16:10:49 +0800

    add web

--- train.py
+++
@@ -1,7 +0,0 @@
-import os.path as osp
-import basicsr
-import diffglv
-
-if __name__ == '__main__':
-    root_path = osp.abspath(osp.join(__file__, osp.pardir))
-    basicsr.train_pipeline(root_path)
